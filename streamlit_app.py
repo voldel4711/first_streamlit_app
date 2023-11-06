e51ccd92d059329359106341da9eaec36570237b
voldel4711 <150057525+voldel4711@users.noreply.github.com>
2023-11-06 17:22:01 +0100

    Update streamlit_app.py
    Apostroph added after error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu)
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
